4323c63fdcfffc095902b143dcd36763144d1bc7
Lorraine <limgytjd98@gmail.com>
2022-01-02 02:05:27 +0900

    install webpack and css, image

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import "./style.css";
+import IMAGE from "./dayzLogo.png";
+import LOGO from "./dayz.svg";
 export const App = () => {
-  return <h1>hello to hyosung world!</h1>;
+  return (
+    <>
+      <h1>hello to hyosung world!</h1>;
+      <img src={IMAGE} />
+      <img src={LOGO} />
+    </>
+  );
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,28 +2,46 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/test.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname + "/build")
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    //Resolve는 가져온 모듈을 찾는 데 도움이 되는 Webpack 라이브러리.
+    resolve: {
+        //배열에 적힌 순서대로 확장자를 찾음.
+        extensions: ['.tsx', '.ts', '.js'],
     },
-    mode: "none",
     module: {
+        //node modules를 제외하고 ts, js, jsx, tsx 파일을 실행할 땐 babel-loader를 통해 실행하도록 지정.
         rules: [
             {
-                test: /\.html$/,
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
                 use: [
                     {
-                        loader: "html-loader",
-                        options: { minimize: true }
+                        loader: 'babel-loader'
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
             }
         ]
     },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
     plugins: [
         new HtmlWebPackPlugin({
-            template: './public/index.html', // public/index.html 파일을 읽는다.
-            filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
+            template: path.resolve(__dirname, '..', './src/index.html')
         })
     ]
+}
